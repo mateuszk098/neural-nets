53408c6b3cf8492f27530670d09197a7a2a9757e
Mateusz Kowalczyk <mkowalczyk013@gmail.com>
2025-01-21 20:04:54 +0100

    Update

---
+++ src/cvnets/yolov2/utils.py
@@ -0,0 +1,65 @@
+import torch
+from torch.types import Tensor
+
+
+def xyxy2xywh(bboxes: Tensor) -> Tensor:
+    xywh = torch.zeros_like(bboxes)
+    xywh[..., :2] = bboxes[..., :2] + 0.5 * (bboxes[..., 2:] - bboxes[..., :2])
+    xywh[..., 2:] = bboxes[..., 2:] - bboxes[..., :2]
+    return xywh
+
+
+def xywh2xyxy(bboxes: Tensor) -> Tensor:
+    xyxy = torch.zeros_like(bboxes)
+    xyxy[..., :2] = bboxes[..., :2] - 0.5 * bboxes[..., 2:]
+    xyxy[..., 2:] = bboxes[..., :2] + 0.5 * bboxes[..., 2:]
+    return xyxy
+
+
+def iou(bboxes1: Tensor, bboxes2: Tensor) -> Tensor:
+    x1 = torch.max(bboxes1[..., 0], bboxes2[..., 0])
+    y1 = torch.max(bboxes1[..., 1], bboxes2[..., 1])
+    x2 = torch.min(bboxes1[..., 2], bboxes2[..., 2])
+    y2 = torch.min(bboxes1[..., 3], bboxes2[..., 3])
+
+    intersection = (x2 - x1).clamp(0) * (y2 - y1).clamp(0)
+    area1 = (bboxes1[..., 2] - bboxes1[..., 0]) * (bboxes1[..., 3] - bboxes1[..., 1])
+    area2 = (bboxes2[..., 2] - bboxes2[..., 0]) * (bboxes2[..., 3] - bboxes2[..., 1])
+
+    return intersection / (area1 + area2 - intersection + 1e-6)
+
+
+def anchor_iou(whs1: Tensor, whs2: Tensor) -> Tensor:
+    area1 = whs1[..., 0] * whs1[..., 1]
+    area2 = whs2[..., 0] * whs2[..., 1]
+    intersection = torch.min(whs1[..., 0], whs2[..., 0]) * torch.min(whs1[..., 1], whs2[..., 1])
+    return intersection / (area1 + area2 - intersection + 1e-6)
+
+
+def create_offsets(S: int) -> tuple[Tensor, Tensor]:
+    cys, cxs = torch.meshgrid(torch.arange(S) / S, torch.arange(S) / S, indexing="ij")
+    cys = cys.reshape(1, S, S, 1).repeat(1, 1, 1, 5)
+    cxs = cxs.reshape(1, S, S, 1).repeat(1, 1, 1, 5)
+    return cys, cxs
+
+
+def decode_preds(preds: Tensor, anchor_bboxes: Tensor, S: int, imgsz: int) -> Tensor:
+    cys, cxs = create_offsets(S)
+    anchors = anchor_bboxes.reshape(1, 1, 1, 5, 2).repeat(1, S, S, 1, 1)
+    decoded = torch.zeros_like(preds)
+
+    decoded[..., 0] = cxs + preds[..., 0] / S
+    decoded[..., 1] = cys + preds[..., 1] / S
+    decoded[..., 2] = anchors[..., 0] * torch.exp(preds[..., 2])
+    decoded[..., 3] = anchors[..., 1] * torch.exp(preds[..., 3])
+
+    decoded[..., :4] = xywh2xyxy(decoded[..., :4]) * imgsz
+    decoded[..., 4:] = preds[..., 4:]
+
+    return decoded
+
+
+def decode_target(target: Tensor, anchor_bboxes: Tensor, S: int, imgsz: int) -> Tensor:
+    decoded = decode_preds(target, anchor_bboxes, S, imgsz)
+    decoded[..., :4] = torch.masked_fill(decoded[..., :4], target[..., :4] == 0, 0)
+    return decoded
--- src/cvnets/yolov2/voc.py
+++ src/cvnets/yolov2/voc.py
@@ -1,8 +1,216 @@
+import os
+from collections import namedtuple
+from os import PathLike
+from pathlib import Path
+from typing import Literal, Self
+from xml.etree import ElementTree
+
+os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
+
+import albumentations as A
+import cv2 as cv
+import torch
 from torch.types import Tensor
+from torch.utils.data import Dataset
 
-from cvnets.yolov1.voc import VOCDataset as YOLOv1VOCDataset
+from cvnets.yolov2.utils import anchor_iou, xyxy2xywh
+
+Detection = namedtuple("Detection", ("label", "bbox"))
+ImageMeta = namedtuple("ImageMeta", ("path", "width", "height", "detections"))
+YOLOSample = namedtuple("YOLOSample", ("image", "bboxes", "labels", "target"))
 
 
-class VOCDataset(YOLOv1VOCDataset):
+def collate_fn(samples: list[YOLOSample]) -> tuple[Tensor, Tensor, list[Tensor], list[Tensor]]:
+    batch_images = list()
+    batch_labels = list()
+    batch_bboxes = list()
+    batch_targets = list()
+
+    for image, bboxes, labels, target in samples:
+        batch_images.append(image)
+        batch_labels.append(labels)
+        batch_bboxes.append(bboxes)
+        batch_targets.append(target)
+
+    batch_images = torch.stack(batch_images, dim=0)
+    batch_targets = torch.stack(batch_targets, dim=0)
+
+    return batch_images, batch_targets, batch_bboxes, batch_labels
+
+
+class VOCDataset(Dataset):
+    def __init__(
+        self,
+        root: str | PathLike,
+        /,
+        *,
+        anchor_bboxes: Tensor,
+        downsample: int = 2**5,
+        imgsz: int = 416,
+        split: Literal["train", "val"],
+        normalize: bool = True,
+    ) -> None:
+        dataset, classes = self.load(root, split)
+
+        self.dataset = dataset
+        self.class_to_idx = {cls: idx for idx, cls in enumerate(sorted(classes))}
+        self.idx_to_class = {idx: cls for cls, idx in self.class_to_idx.items()}
+
+        self.anchor_bboxes = anchor_bboxes.clone()
+        self.downsample = int(downsample)
+
+        self.B = anchor_bboxes.size(0)
+        self.C = len(self.class_to_idx)
+        self.normalize = bool(normalize)
+
+        self._imgsz = int(imgsz)
+        self.S = self.imgsz // self.downsample
+
+        self._train_transform = A.Compose(
+            [
+                A.HorizontalFlip(p=0.5),
+                A.CropAndPad(percent=[-0.25, 0.25], p=1),
+                A.Affine(
+                    scale=(0.75, 1.25),
+                    border_mode=cv.BORDER_REPLICATE,
+                    fit_output=True,
+                    keep_ratio=True,
+                    p=1,
+                ),
+                A.ColorJitter(
+                    brightness=(0.75, 1.25),
+                    contrast=(0.75, 1.25),
+                    saturation=(0.75, 1.25),
+                    hue=(-0.25, 0.25),
+                    p=1,
+                ),
+                A.ToGray(num_output_channels=3, p=0.1),
+                A.Resize(self.imgsz, self.imgsz),
+            ],
+            bbox_params=A.BboxParams(format="pascal_voc", label_fields=["labels"], min_visibility=0.25),
+            seed=42,
+        )
+
+        self._eval_transform = A.Compose(
+            [
+                A.Resize(self.imgsz, self.imgsz),
+            ],
+            bbox_params=A.BboxParams(format="pascal_voc", label_fields=["labels"]),
+            seed=42,
+        )
+
+        if self.normalize:  # Useful when using non-normalized images for testing.
+            self._train_transform.transforms.append(A.Normalize())
+            self._eval_transform.transforms.append(A.Normalize())
+
+        self.transform = self._eval_transform
+
+    @property
+    def imgsz(self) -> int:
+        return self._imgsz
+
+    @imgsz.setter
+    def imgsz(self, value: int) -> None:
+        self._imgsz = int(value)
+        self.S = self.imgsz // self.downsample
+
+        for transform in (self._train_transform, self._eval_transform):
+            for idx, t in enumerate(transform.transforms):
+                if isinstance(t, A.Resize):
+                    transform.transforms[idx] = A.Resize(value, value)
+                    break
+
+    def __len__(self) -> int:
+        return len(self.dataset)
+
+    def __getitem__(self, index: int) -> YOLOSample:
+        image_meta = self.dataset[index]
+
+        image = cv.imread(str(image_meta.path), cv.IMREAD_COLOR)
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+
+        bboxes = list(d.bbox for d in image_meta.detections)
+        labels = list(self.class_to_idx[d.label] for d in image_meta.detections)
+
+        transformed = self.transform(image=image, bboxes=bboxes, labels=labels)
+
+        image = torch.from_numpy(transformed["image"]).permute(2, 0, 1).float()  # C, H, W
+        bboxes = torch.as_tensor(transformed["bboxes"]).float()
+        labels = torch.as_tensor(transformed["labels"]).int()
+
+        if not bboxes.any():  # If all bboxes are removed by augmentations, try again.
+            return self.__getitem__(index)
+
+        target = self._create_yolo_target(bboxes, labels)
+
+        return YOLOSample(image, bboxes, labels, target)
+
     def _create_yolo_target(self, bboxes: Tensor, labels: Tensor) -> Tensor:
-        raise NotImplementedError("Implement this for YOLOv2 loss.")
+        target = torch.zeros((self.S, self.S, self.B, 5 + self.C))
+        bboxes_xywh = xyxy2xywh(bboxes)
+
+        cell_size = self.imgsz // self.S
+        ns, ms = (bboxes_xywh[:, :2] / cell_size).int().t()
+        txys = (bboxes_xywh[:, :2] % cell_size) / cell_size
+        whs = bboxes_xywh[:, 2:] / self.imgsz
+
+        for idx, (n, m, txy, wh) in enumerate(zip(ns, ms, txys, whs)):
+            k = torch.argmax(anchor_iou(wh, self.anchor_bboxes))
+            twh = torch.log(wh / self.anchor_bboxes[k])
+
+            target[m, n, k, 0:2] = txy
+            target[m, n, k, 2:4] = twh
+            target[m, n, k, 4] = 1.0  # IoU of the predicted bbox with the ground truth bbox.
+            target[m, n, k, 5 + labels[idx]] = 1.0  # One-hot encode the class label.
+
+        return target
+
+    def train(self) -> Self:
+        self.transform = self._train_transform
+        return self
+
+    def eval(self) -> Self:
+        self.transform = self._eval_transform
+        return self
+
+    @staticmethod
+    def load(root: str | PathLike, split: Literal["train", "val"]) -> tuple[list[ImageMeta], set[str]]:
+        data_root = Path(root).expanduser()
+
+        dataset = list()
+        classes = set()
+
+        for voc in data_root.iterdir():
+            image_set = voc.joinpath("ImageSets", "Main", f"{split}.txt")
+
+            with open(image_set) as f:
+                for name in (name.strip() for name in f):
+                    image_path = voc.joinpath("JPEGImages", f"{name}.jpg")
+                    annot_path = voc.joinpath("Annotations", f"{name}.xml")
+
+                    annot_tree = ElementTree.parse(annot_path)
+                    annot_root = annot_tree.getroot()
+
+                    detections = list()
+                    # Following elements are always present, so don't mind about the type ignore.
+                    for obj in annot_root.findall("object"):
+                        if obj.find("difficult").text == "1":  # type: ignore
+                            continue
+
+                        label = obj.find("name").text  # type: ignore
+                        bndbox = obj.find("bndbox")  # type: ignore
+                        bbox = (
+                            int(bndbox.find("xmin").text),  # type: ignore
+                            int(bndbox.find("ymin").text),  # type: ignore
+                            int(bndbox.find("xmax").text),  # type: ignore
+                            int(bndbox.find("ymax").text),  # type: ignore
+                        )
+                        detections.append(Detection(label, bbox))
+                        classes.add(label)
+
+                    if detections:
+                        width = int(annot_root.find("size").find("width").text)  # type: ignore
+                        height = int(annot_root.find("size").find("height").text)  # type: ignore
+                        dataset.append(ImageMeta(image_path, width, height, detections))
+
+        return dataset, classes
